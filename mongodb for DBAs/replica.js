dece0ab364f1037b4fc28ca538d21f50fd2de5f7
Ada Lee <adalee2future@users.noreply.github.com>
2015-02-04 19:13:16 +0800

    Replica Set

--- mongodb for DBAs/replica.js
+++ mongodb for DBAs/replica.js
@@ -1,26 +1,23 @@
-mkdir dba
-cd dba
-mkdir 1 2 3
-mongod --replSet abc --dbpath 1 --port 27001 --oplogSize 50 --logpath log.1 --logappend --fork 
-mongod --replSet abc --dbpath 2 --port 27001 --oplogSize 50 --logpath log.2 --logappend --fork 
-mongod --replSet abc --dbpath 3 --port 27001 --oplogSize 50 --logpath log.3 --logappend --fork 
+// on the shell
+mongod  --replSet abc --dbpath 1 --port 27001 --oplogSize 50 -logpath log.1 --logappend --fork
+mongod  --replSet abc --dbpath 2 --port 27002 --oplogSize 50 -logpath log.2 --logappend --fork
+mongod  --replSet abc --dbpath 3 --port 27003 --oplogSize 50 -logpath log.3 --logappend --fork
+ps
+ps -ef
 
-cfg = 
-{
-	"_id" : "abc",
-	"members" : [
-		{
-			"_id" : 0,
-			"host" : "localhost:27001"
-		},
-		{
-			"_id" : 1,
-			"host" : "localhost:27002"
-		},
-		{
-			"_id" : 2,
-			"host" : "localhost:27003"
-		}
-	]
+mongo --port 27001
+
+// in mongo shell
+cfg = {
+    _id : "abc",
+    members : [
+        { _id:0, host: "localhost:27001"},
+        { _id:1, host: "localhost:27002"},
+        { _id:2, host: "localhost:27003"}
+    ]
 }
-rs.help()
+
+
+rs.initiate(cfg)
+
+rs.status()
