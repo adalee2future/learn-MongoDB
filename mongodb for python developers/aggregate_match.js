16f7d5e089830e031da5afd481eaec3db964e401
Ada Lee <adalee2future@users.noreply.github.com>
2015-02-05 22:12:50 +0800

    Aggregate : Using $match

--- mongodb for python developers/aggregate_match.js
+++ mongodb for python developers/aggregate_match.js
@@ -12,7 +12,22 @@ db.zips.aggregate([
       }
     }
 ])
-
 //{ "_id" : "94301", "city" : "PALO ALTO", "loc" : [ -122.149685, 37.444324 ], "pop" : 15965, "state" : "CA" }
 //{ "_id" : "94304", "city" : "PALO ALTO", "loc" : [ -122.184234, 37.433424 ], "pop" : 1835, "state" : "CA" }
 //{ "_id" : "94306", "city" : "PALO ALTO", "loc" : [ -122.127375, 37.418009 ], "pop" : 24309, "state" : "CA" }
+ 
+db.zips.aggregate([
+    {$match:
+      {
+        state : "CA"
+      }
+    },
+    {$group:
+      {
+        _id: "$city",
+        population: {$sum: "$pop"},
+        zip_codes: {$addToSet: "$_id"}
+      } 
+    }          
+])
+//{ "_id" : "PALO ALTO", "population" : 42109, "zip_codes" : [ "94306", "94304", "94301" ] }
