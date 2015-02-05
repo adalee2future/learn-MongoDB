b6cef0b155cceca78f86adc85309035d2fe6b560
Ada Lee <adalee2future@users.noreply.github.com>
2015-02-05 21:09:47 +0800

    Double $group stages

--- mongodb for python developers/double_group.js
+++ mongodb for python developers/double_group.js
@@ -9,14 +9,16 @@ db.grades.find( { } , { _id : 0 } )
 //{ "class" : "Math", "student" : "234", "score" : 4 }
 
 // one group
-db.grades.aggregate( [ { $group : { _id : { student : "$student" , class : "$class"} , average : { $avg : "$score"}}}])
+db.grades.aggregate( [ { $group : { _id : { student : "$student" , class : "$class"} ,
+                                    average : { $avg : "$score"}}}])
 //{ "_id" : { "student" : "234", "class" : "Math" }, "average" : 3.75 }
 //{ "_id" : { "student" : "123", "class" : "Writting" }, "average" : 4.5 }
 //{ "_id" : { "student" : "234", "class" : "CS" }, "average" : 4.5 }
 //{ "_id" : { "student" : "123", "class" : "Math" }, "average" : 3.25 }
 
 // double group
-db.grades.aggregate( [ { $group : { _id : { student : "$student" , class : "$class"} , average : { $avg : "$score"}}} , {$group : {_id : "$_id.class", average : {$avg : "$average"}}} ])
+db.grades.aggregate( [ { $group : { _id : { student : "$student" , class : "$class"} ,average : { $avg : "$score"}}} ,
+                       { $group : {_id : "$_id.class", average : {$avg : "$average"}}} ])
 //{ "_id" : "CS", "average" : 4.5 }
 //{ "_id" : "Writting", "average" : 4.5 }
 //{ "_id" : "Math", "average" : 3.5 }
